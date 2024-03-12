2e60e3a43eefa53edc2415487747e25ad2518906
Shamainb <shamainbhengesa60@gmail.com>
2024-03-12 20:18:37 +0200

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,7 +16,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    return "C {}".format(text.replace('_', ' '))
+    return "C "  + text.replace('_', ' ')
 
 if __name__ == ' __main__':
     app.run(host='0.0.0.0', port=5000)
