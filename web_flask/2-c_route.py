2e269cad7b9a2c09858cb77ef344c019438a32da
Shamainb <shamainbhengesa60@gmail.com>
2024-03-12 18:45:03 +0000

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-from flask import Flask
-
 """
 a script that starts a Flask web application
 """
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_is_fun(text):
-    return "C "  + text.replace('_', ' ')
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
 
-if __name__ == ' __main__':
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
