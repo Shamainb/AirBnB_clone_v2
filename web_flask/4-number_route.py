9166a992cb1cef42a6c2f796f33a5bf912944185
Shamainb <shamainbhengesa60@gmail.com>
2024-03-14 04:42:51 +0200

    Is it a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 """
-a script that starts a Flask web application
+starts a Flask web application
 """
-from flask import Flask, request
 
+from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+def index():
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    text = text.replace('_', ' ')
-    return "C " + text
+def cisfun(text):
+    return 'C ' + text.replace('_', ' ')
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    text = text.replace('_', ' ')
-    return "Python " + text
+def pythoniscool(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return "{} is a number".format(n)
+def imanumber(n):
+    return "{:d} is a number".format(n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
