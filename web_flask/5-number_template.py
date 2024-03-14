68e75dd95d9e64fe3b1e819e517a1900c1933081
Shamainb <shamainbhengesa60@gmail.com>
2024-03-14 16:59:28 +0200

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -11,9 +11,11 @@ app = Flask(__name__)
 def index():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
@@ -25,13 +27,16 @@ def cisfun(text):
 def pythoniscool(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def imanumber(n):
     return "{:d} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbersandtemplates(n):
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
